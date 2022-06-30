05d4f122f0ab25a00a9e805ba1171504c3d10c62
ahbko <annavenevtseva@yahoo.com>
2022-06-30 17:56:36 +0300

    Tasks from the 2nd section of the course.

---
+++ 2_Useful methods of Selenium tasks/2_lesson3_step4.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "http://suninjuly.github.io/alert_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button1 = browser.find_element(By.TAG_NAME, "button")
+    button1.click()
+    confirm = browser.switch_to.alert
+    confirm.accept()
+    x_element = browser.find_element(By.ID, "input_value")
+    x = x_element.text
+    y = calc(x)
+    input1 = browser.find_element(By.ID, "answer")
+    input1.send_keys(y)
+    button2 = browser.find_element(By.TAG_NAME, "button")
+    button2.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
